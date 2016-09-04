9579624a811a76b233b7efd33392056fbea30851
Alexander <yumako87@gmail.com>
2016-09-04 15:39:13 +0200

    Delete require params to see if comments work

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -36,7 +36,7 @@ class CommentsController < ApplicationController
     end
 
     def comment_params
-      params.require(:comment).permit(:content,:user_id,:post_id)
+      params.require(:comment).permit(:content)
     end
 
 
