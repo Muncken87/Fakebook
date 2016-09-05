1b717e0b6c18e5e5c0ef057621f89f0d3a5c569b
Alexander <yumako87@gmail.com>
2016-09-05 17:03:52 +0200

    Pushing changes for laptop

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,6 @@
 class Post < ApplicationRecord
    belongs_to :user
-   has_many :comments
+   has_many :comments, dependent: :delete_all
    mount_uploader :image, AvatarUploader
    validates :content, presence: true, length: {minimum: 5}
    delegate :username, :username=, :email, :email=,:avatar, :avatar=, :to => :user, allow_nil: true
